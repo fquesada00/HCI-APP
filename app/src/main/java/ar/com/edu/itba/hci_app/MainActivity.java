5564b8144fa7e679b7993b33a3e2a3e33d2dddf1
Octavio Serpe <oserpe@itba.edu.ar>
2020-11-15 12:15:27 -0300

    bottom nav bar funcionando pero sin el comportamiento de yt/ig, fragmentos con patron singleton, destruccion de dispatcher con back

--- app/src/main/java/ar/com/edu/itba/hci_app/MainActivity.java
+++ app/src/main/java/ar/com/edu/itba/hci_app/MainActivity.java
@@ -2,13 +2,32 @@ package ar.com.edu.itba.hci_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("StaticFieldLeak")
+    private static Button secondActBtn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        secondActBtn = findViewById(R.id.second_activity_btn);
+        secondActBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SecondActivity.class));
+                setContentView(R.layout.second_activity);
+                finish();
+            }
+        });
     }
+
+
 }
