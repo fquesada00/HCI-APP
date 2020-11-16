059e66ab2d9745dcfebb5a06209ac082f9eb2624
Octavio Serpe <oserpe@itba.edu.ar>
2020-11-15 23:22:15 -0300

    pusheo de models unicamente

---
+++ app/src/main/java/ar/com/edu/itba/hci_app/network/api/model/Rating.java
@@ -0,0 +1,102 @@
+package ar.com.edu.itba.hci_app.network.api.model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Rating {
+
+    @SerializedName("totalCount")
+    @Expose
+    private int totalCount;
+    @SerializedName("orderBy")
+    @Expose
+    private String orderBy;
+    @SerializedName("direction")
+    @Expose
+    private String direction;
+    @SerializedName("results")
+    @Expose
+    private List<Routine> routines = null;
+    @SerializedName("size")
+    @Expose
+    private int size;
+    @SerializedName("page")
+    @Expose
+    private int page;
+    @SerializedName("isLastPage")
+    @Expose
+    private boolean isLastPage;
+
+
+    public Rating() {
+    }
+
+    public Rating(int totalCount, String orderBy, String direction, List<Routine> routines, int size, int page, boolean isLastPage) {
+        super();
+        this.totalCount = totalCount;
+        this.orderBy = orderBy;
+        this.direction = direction;
+        this.routines = routines;
+        this.size = size;
+        this.page = page;
+        this.isLastPage = isLastPage;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public List<Routine> getRoutines() {
+        return routines;
+    }
+
+    public void setRoutines(List<Routine> routines) {
+        this.routines = routines;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public boolean isIsLastPage() {
+        return isLastPage;
+    }
+
+    public void setIsLastPage(boolean isLastPage) {
+        this.isLastPage = isLastPage;
+    }
+
+}
---
+++ app/src/main/java/ar/com/edu/itba/hci_app/network/api/model/Routine.java
@@ -0,0 +1,126 @@
+package ar.com.edu.itba.hci_app.network.api.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Routine {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("detail")
+    @Expose
+    private String detail;
+    @SerializedName("dateCreated")
+    @Expose
+    private int dateCreated;
+    @SerializedName("averageRating")
+    @Expose
+    private int averageRating;
+    @SerializedName("isPublic")
+    @Expose
+    private boolean isPublic;
+    @SerializedName("difficulty")
+    @Expose
+    private String difficulty;
+    @SerializedName("creator")
+    @Expose
+    private Creator creator;
+    @SerializedName("category")
+    @Expose
+    private Category category;
+
+
+    public Routine() {
+    }
+
+
+    public Routine(int id, String name, String detail, int dateCreated, int averageRating, boolean isPublic, String difficulty, Creator creator, Category category) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.detail = detail;
+        this.dateCreated = dateCreated;
+        this.averageRating = averageRating;
+        this.isPublic = isPublic;
+        this.difficulty = difficulty;
+        this.creator = creator;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(int dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public int getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(int averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public boolean isIsPublic() {
+        return isPublic;
+    }
+
+    public void setIsPublic(boolean isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Creator getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Creator creator) {
+        this.creator = creator;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+}
