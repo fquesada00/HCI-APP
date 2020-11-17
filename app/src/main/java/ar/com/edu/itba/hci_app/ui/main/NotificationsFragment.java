8f5b64fe9bb1310e7089ac0bd85f831f7dc86dff
gastondonikian <gastondonikian@gmail.com>
2020-11-17 18:17:42 -0300

    Commit De Nuevos Fragmentos antes de pullear

--- app/src/main/java/ar/com/edu/itba/hci_app/ui/main/NotificationsFragment.java
+++ app/src/main/java/ar/com/edu/itba/hci_app/ui/main/NotificationsFragment.java
@@ -7,7 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.RatingBar;
 import ar.com.edu.itba.hci_app.R;
 import ar.com.edu.itba.hci_app.databinding.FragmentNotificationsBinding;
 import ar.com.edu.itba.hci_app.repository.BaseRepository;
