572cecf57b61ad4cc8af95c3676a7bdee2938b94
gastondonikian <gastondonikian@gmail.com>
2020-11-18 22:03:57 -0300

    Commit para pull

--- app/src/main/java/ar/com/edu/itba/hci_app/ui/main/RoutineDescriptionFragment.java
+++ app/src/main/java/ar/com/edu/itba/hci_app/ui/main/RoutineDescriptionFragment.java
@@ -2,28 +2,36 @@ package ar.com.edu.itba.hci_app.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ar.com.edu.itba.hci_app.R;
+import ar.com.edu.itba.hci_app.ui.base.BaseFragment;
 
 
-public class RoutineDescriptionFragment extends Fragment {
-
+public class RoutineDescriptionFragment extends BaseFragment {
 
     public RoutineDescriptionFragment() {
         // Required empty public constructor
     }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_routine_description, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
